36dcfeffe8538a77ecc14d62c71b58e6a51033fa
Dan Clark <danclark@redhat.com>
2020-06-10 13:04:17 -0400

    Update kernel version. Build as root to get subscriptions

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
+#!/bin/bash -xe
 
 # May need to disable SELinux?
 
-KERNEL_VERSION=4.18.0-147.5.1.el8_1.x86_64
+#KERNEL_VERSION=4.18.0-147.5.1.el8_1.x86_64
+KERNEL_VERSION=4.18.0-147.8.1.el8_1.x86_64
 DRIVER_VERSION=440.64.00
+RHCOS_VERSION=4.4
 
-podman build -t quay.io/danclark/nvidia-driver:${DRIVER_VERSION}-1.0.0-custom-rhcos4.3 -f Dockerfile .
+sudo podman build -t quay.io/danclark/nvidia-driver:${DRIVER_VERSION}-1.0.0-custom-rhcos${RHCOS_VERSION} -f Dockerfile .
 
 exit 0
