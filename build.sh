b8075157a8c3d8f61341b53665cb325c8aa30b8c
Dan Clark <danclark@redhat.com>
2020-09-10 14:38:11 +0000

    Make registry and repo configurable

--- build.sh
+++ build.sh
@@ -6,7 +6,9 @@
 KERNEL_VERSION=4.18.0-147.8.1.el8_1.x86_64
 DRIVER_VERSION=440.64.00
 RHCOS_VERSION=4.4
+REGISTRY="quay.io"
+REPO="danclark"
 
-sudo podman build -t quay.io/danclark/nvidia-driver:${DRIVER_VERSION}-1.0.0-custom-rhcos${RHCOS_VERSION} -f Dockerfile .
+sudo podman build -t ${REGISTRY}/${REPO}/nvidia-driver:${DRIVER_VERSION}-1.0.0-custom-rhcos${RHCOS_VERSION} -f Dockerfile .
 
 exit 0
