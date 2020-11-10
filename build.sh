9e6e1d89963d91017ec9f049196d7a1f74923328
Dan Clark <danclark@redhat.com>
2020-11-10 21:00:00 +0000

    Determine kernel version from RHCOS version

--- build.sh
+++ build.sh
@@ -2,13 +2,15 @@
 
 # See the README file for a description of these variables
 BASE_URL='https://us.download.nvidia.com/tesla'
-KERNEL_VERSION='4.18.0-147.3.1.el8_1.x86_64'
+#KERNEL_VERSION='4.18.0-193.13.2.el8_2.x86_64'
 DRIVER_VERSION='450.51.06'
-RHCOS_VERSION='4.3.0'
+RHCOS_VERSION='4.5.6'
 REGISTRY='quay.io'
 REPO='danclark'
 
-sudo podman build \
+KERNEL_VERSION=$(grep ${RHCOS_VERSION} README.md | awk -F\| '{print $3}' | tr -d ' ')
+
+sudo podman build --no-cache \
      --tag ${REGISTRY}/${REPO}/nvidia-driver:${DRIVER_VERSION}-1.0.0-custom-rhcos-${KERNEL_VERSION}-${RHCOS_VERSION} \
      --build-arg=DRIVER_VERSION=${DRIVER_VERSION} \
      --build-arg=BASE_URL=https://us.download.nvidia.com/tesla \
